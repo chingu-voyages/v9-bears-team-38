1ac3058ca708bb0fe104cfede8808677cf27342e
ZumDeWald <zumdewald@gmail.com>
2019-06-02 21:49:13 -0500

    Added favicon and title

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div title='app-container'>
-        <h1>Boilerplate!!</h1>
+        <h1>Chingu Video Portal</h1>
       </div>
     );
   }
