300d24d5b0fd4bbb9271d62c61f9990714d62e20
ZumDeWald <zumdewald@gmail.com>
2019-06-13 23:10:24 -0500

    Add search and admin login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,6 +15,14 @@ export default class App extends Component {
             className='logo pm0'
           />
           <h1 className='b pm0 tc main-title'>Chingu Learning Portal</h1>
+          <input
+            type='text'
+            id='search'
+            name='search'
+            aria-label='Search'
+            placeholder='Search videos ...'
+          />
+          <p className='tc pointer'>Admin Login</p>
         </header>
         <nav className='lbgc fbc nav'>
           <h2 className='b playlists'>Playlists</h2>
@@ -24,9 +32,9 @@ export default class App extends Component {
             <li>Voyage 8</li>
           </ul>
         </nav>
-        <main>
-          <section className='most-recent' />
-          <section className='other-vids' />
+        <main className='fbc'>
+          <section className='most-recent'>MOST RECENT VID</section>
+          <section className='other-vids'>OTHER VIDS</section>
         </main>
       </div>
     );
