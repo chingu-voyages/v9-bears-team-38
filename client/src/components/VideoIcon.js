778beb26a3f79c73e93213a28f8001b79f0973ba
ZumDeWald <zumdewald@gmail.com>
2019-06-24 22:34:25 -0500

    Initial layout with dynamic icons

--- client/src/components/VideoIcon.js
+++ client/src/components/VideoIcon.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const VideoIcon = ({category}) => {
+  //Choose correct icon for category
+  const cat = category;
+  let switchOutput;
+
+  switch (cat) {
+    case 'AMA':
+      switchOutput = 'fa-comments';
+      break;
+    default:
+      switchOutput = 'fa-question-circle';
+  }
+
   return (
     <span className='fbc pm0 tile-icon'>
-      <i className='far fa-question-circle fa-2x' />
+      <i className={`far ${switchOutput} fa-2x`} />
       <span className='pm0'>{category}</span>
     </span>
   );
