b9cf4d9c9f862e6c6491c07cf1dc2b8908ec2bb9
ZumDeWald <zumdewald@gmail.com>
2019-07-11 21:42:53 -0500

    Main vid style good for large screens

--- client/src/components/VidTile.js
+++ client/src/components/VidTile.js
@@ -6,19 +6,23 @@ import '../styles/VidTile.css';
 
 const VidTile = ({vidObj}) => {
   return (
-    <section className='fbc vid-tile'>
+    <section className='fbr vid-tile'>
+      <h3 className='vid-title'>{vidObj.title}</h3>
       <iframe
         src={vidObj.url + '?showinfo=0'}
-        className='vid-src'
+        className='main-src'
         frameBorder='0'
         allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
         allowFullScreen
       />
-      <div className='vid-info'>
-        <h3 className='vid-title'>{vidObj.title}</h3>
-        <p className='vid-desc'>{vidObj.description}</p>
-        <VideoIcon classame='main-icon' category={vidObj.category} />
-        <span className='pm0 vid-tags'>Tags: {vidObj.tags}</span>
+      <div className='fbc vid-info'>
+        <p className='pm0 vid-desc'>{vidObj.description}</p>
+        <span className='pm0 vid-tags'>
+          <strong>Starring:</strong> {vidObj.starring}
+        </span>
+        <span className='pm0 vid-tags'>
+          <strong>Tags:</strong> {vidObj.tags}
+        </span>
       </div>
     </section>
   );
